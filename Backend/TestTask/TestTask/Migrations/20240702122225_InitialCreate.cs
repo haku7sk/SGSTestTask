d5ff15cc8a9c80bf16e4ec9d5806935b7c11edf8
haku7sk <yerassylmuratbekov@Yerassyls-MacBook-Air.local>
2024-07-02 17:43:59 +0500

    add ef core migrations

---
+++ Backend/TestTask/TestTask/Migrations/20240702122225_InitialCreate.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TestTask.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Containers",
+                columns: table => new
+                {
+                    ID = table.Column<Guid>(type: "uuid", nullable: false),
+                    Number = table.Column<int>(type: "integer", nullable: false),
+                    Type = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    Length = table.Column<decimal>(type: "numeric(10,2)", nullable: false),
+                    Width = table.Column<decimal>(type: "numeric(10,2)", nullable: false),
+                    Height = table.Column<decimal>(type: "numeric(10,2)", nullable: false),
+                    Weight = table.Column<decimal>(type: "numeric(10,2)", nullable: false),
+                    IsEmpty = table.Column<bool>(type: "boolean", nullable: false),
+                    DateReceived = table.Column<DateTime>(type: "timestamp", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Containers", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Operations",
+                columns: table => new
+                {
+                    ID = table.Column<Guid>(type: "uuid", nullable: false),
+                    ContainerID = table.Column<Guid>(type: "uuid", nullable: false),
+                    StartDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    EndDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    OperationType = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    OperatorName = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: false),
+                    InspectionPlace = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Operations", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_Operations_Containers_ContainerID",
+                        column: x => x.ContainerID,
+                        principalTable: "Containers",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Operations_ContainerID",
+                table: "Operations",
+                column: "ContainerID");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Operations");
+
+            migrationBuilder.DropTable(
+                name: "Containers");
+        }
+    }
+}
